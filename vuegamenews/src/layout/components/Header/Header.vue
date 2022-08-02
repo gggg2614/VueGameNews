c56b9425623c7e37cea61dffbb1423d697fe4f30
IMliuyi <0>
2022-08-02 16:02:58 +0800

    layout

--- vuegamenews/src/layout/components/Header/Header.vue
+++ vuegamenews/src/layout/components/Header/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="header">
-    <el-row   >
-  <el-col :span="10" ><i class="el-icon-caret-right"></i>VueGamesDemo</el-col>
-  <el-col :span="13" ><i class="el-icon-user-solid"></i> 登录</el-col>
-  <el-col :span="1" :pull="6">注册</el-col>
+    <el-row  :gutter="32">
+  <el-col :span="10" :xs="12" :sm="10" ><i class="el-icon-caret-right"></i>VueGamesDemo</el-col>
+  <el-col :span="13" :xs="4" :sm="8" ><i class="el-icon-user-solid"></i> 登录</el-col>
+  <el-col :span="1" :xs="2" :sm="5" :pull="5" >注册</el-col>
     </el-row>
   </div>
 </template>
